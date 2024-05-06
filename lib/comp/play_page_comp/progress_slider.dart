10085a9bfa8d8a037d87c54de9c7d6fc6bcc1425
canxin <q1969730106@gmail.com>
2024-05-06 20:10:18 +0800

    [Fix] use my fork of interactive_slider

--- lib/comp/play_page_comp/progress_slider.dart
+++ lib/comp/play_page_comp/progress_slider.dart
@@ -15,19 +15,19 @@ class ProgressSlider extends StatefulWidget {
 
 // 这个组件代表了一个音乐播放详情页面的播放进度条等内容
 class ProgressSliderState extends State<ProgressSlider> {
-  InteractiveSliderController progressController =
+  final ValueNotifier<bool> _isDragging = ValueNotifier<bool>(false);
+  final InteractiveSliderController _progressController =
       InteractiveSliderController(0);
   late StreamSubscription<double> listen1;
-  bool isPressing = false;
 
   @override
   void initState() {
     super.initState();
     listen1 = globalAudioUiController.playProgress.listen((p0) {
-      if (!isPressing) {
+      if (!_isDragging.value) {
         if (!p0.isNaN) {
           try {
-            progressController.value = p0;
+            _progressController.value = p0;
           } catch (e) {
             if (e.toString().contains("disposed")) {
               listen1.cancel();
@@ -45,23 +45,15 @@ class ProgressSliderState extends State<ProgressSlider> {
       child: GestureDetector(
         child: InteractiveSlider(
           padding: const EdgeInsets.all(0),
-          controller: progressController,
+          controller: _progressController,
           onProgressUpdated: (value) {
             var toSeek = globalAudioUiController.getToSeek(value);
             talker.info(
                 "[Slider] Call seek to ${formatDuration(toSeek.inSeconds)}");
             globalAudioHandler.seek(toSeek);
-            isPressing = false;
           },
+          isDragging: _isDragging,
         ),
-        onTapDown: (_) => setState(() => isPressing = true),
-        onTapUp: (_) => setState(() => isPressing = true),
-        onTapCancel: () => setState(() => isPressing = true),
-        onLongPressStart: (_) => setState(() => isPressing = true),
-        onLongPressEnd: (_) => setState(() => isPressing = true),
-        onPanDown: (_) => setState(() => isPressing = true),
-        onPanCancel: () => setState(() => isPressing = true),
-        onPanEnd: (_) => setState(() => isPressing = true),
       ),
     );
   }
