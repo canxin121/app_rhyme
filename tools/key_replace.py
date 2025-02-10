3c7a11a3cc437143f19ebc97118d38cc03ef82e8
canxin <q1969730106@gmail.com>
2025-02-10 22:22:28 +0800

    [Action] test key_replace

--- tools/key_replace.py
+++ tools/key_replace.py
@@ -2,22 +2,18 @@ import os
 
 
 def main():
-    # Grab environment variables
     secret_key = os.environ.get("SOME_SECRET_KEY")
     target_string = os.environ.get("SOME_REPLACE_KEY")
+    print(len(secret_key), len(target_string))
 
-    # Ensure all variables are present
     if not all([secret_key, "lib/types/plugin.dart", target_string]):
         raise EnvironmentError("Missing one or more required environment variables.")
 
-    # Read the contents of the target file
     with open("lib/types/plugin.dart", "r", encoding="utf-8") as file:
         file_data = file.read()
 
-    # Replace the target string with the secret key
     new_data = file_data.replace(target_string, secret_key)
 
-    # Write the new data back to the file
     with open("lib/types/plugin.dart", "w", encoding="utf-8") as file:
         file.write(new_data)
 
